a1b034e635d68e01e53263924f7e93616da5ef1a
Corpsee <poisoncorpsee@gmail.com>
2014-07-13 15:50:15 +0700

    Added tests for strcspn

--- tests/cases/Utf8StrcspnTest.php
+++ tests/cases/Utf8StrcspnTest.php
@@ -40,4 +40,26 @@ class Utf8StrcspnTest extends PHPUnit_Framework_TestCase
 		$str = "i\nñtërnâtiônàlizætiøn";
 		$this->assertEquals(1, utf8\cspn($str, "\n"));
 	}
+
+	public function test_7()
+	{
+		$str = "abcdefghijklmnop";
+		$this->assertEquals(4, utf8\cspn($str,'h',3));
+		$this->assertEquals(0, utf8\cspn($str,'d',3));
+	}
+
+	public function test_8()
+	{
+		$str = "<b>A brief test.</b><div>";
+		$this->assertEquals(3, utf8\cspn($str, '/>', 21));
+		$this->assertEquals(1, utf8\cspn($str, '/>', 1));
+	}
+
+	public function test_9()
+	{
+		$str = "<b>A brief test.</b><div>現在、企業ユーザー向けに多岐にわたるサービスを提供しています。</div>";
+		$this->assertEquals(2, utf8\cspn($str, ">")); //!
+		$this->assertEquals(3, utf8\cspn($str, '\/\>', 21));
+		$this->assertEquals(8, utf8\cspn($str, '向けに', 26));
+	}
 }
